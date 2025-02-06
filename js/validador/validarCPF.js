a4e920a42a44332d89ddd29c50e0de3fdc32aeb9
REDDEAD <hsesgamer@outlook.com>
2025-02-06 10:54:37 -0300

    Validação de cpf

--- js/validador/validarCPF.js
+++ js/validador/validarCPF.js
@@ -1,23 +1,30 @@
-const CPF = document.getElementById('formulario_CPF')
-const resultado = document.getElementById('resultado__cpf')
-if(validarCPF(CPF.value)){
-resultado.textContent ='CPF válido'
-resultado.style.color = 'green';
-} else{
-    resultado.textContent ='CPF válido'
-resultado.style.color = 'red';
+export function validarCPF(cpf) {
+if (!cpf) {
+alert('CPF indefinido ou vazio')
+return false
+}
+const cpfLimpo = cpf.replace(/\.|-/g, '')
+    console.log('CPF Limpo:', cpfLimpo)
+
+if (validaNumerosRepetidos(cpfLimpo)) {
+    alert('CPF com números repetidos')
+return false
 }
 
- export function validarCPF(cpf){
-    const cpfLimpo = cpf.replace(/\.|-/g, '');
-    if(validaNumerosRepitidos(cpfLimpo) || !validaPrimeiroDigito(cpfLimpo) || !validaSegundoDigito(cpfLimpo)){
-        return false; 
-    } 
-    return true; 
+if (!validaPrimeiroDigito(cpfLimpo)) {
+    alert('Primeiro dígito inválido')
+return false
 }
 
-function validaNumerosRepitidos(cpf){
-    const numerosRepitidos = [
+if (!validaSegundoDigito(cpfLimpo)) {
+    alert('Segundo dígito inválido')
+return false
+}
+return true
+}
+
+function validaNumerosRepetidos(cpf) {
+const numerosRepetidos = [
         '00000000000',
         '11111111111',
         '22222222222',
@@ -28,38 +35,38 @@ function validaNumerosRepitidos(cpf){
         '77777777777',
         '88888888888',
         '99999999999'
-    ];
-    return numerosRepitidos.includes(cpf);
+    ]
+return numerosRepetidos.includes(cpf)
 }
 
-function validaPrimeiroDigito(cpf){
-    let soma = 0;
-    let multiplicador = 10;
+function validaPrimeiroDigito(cpf) {
+let soma = 0
+let multiplicador = 10
 
-    for(let tamanho = 0; tamanho < 9; tamanho++){
-        soma += parseInt(cpf[tamanho]) * multiplicador;
-        multiplicador--;
-    }
-    soma = (soma * 10) % 11;
+for (let i = 0; i < 9; i++) {
+    soma += parseInt(cpf[i]) * multiplicador
+    multiplicador--
+}
+soma = (soma * 10) % 11
 
-    if(soma == 10 || soma == 1){
-        soma = 0; 
-    }
-    return soma == parseInt(cpf[9]);
+if (soma == 10 || soma == 11) {
+    soma = 0
+}
+return soma == parseInt(cpf[9])
 }
 
-function validaSegundoDigito(cpf){
-    let soma = 0;
-    let multiplicador = 11;
+function validaSegundoDigito(cpf) {
+let soma = 0
+    let multiplicador = 11
 
-    for(let tamanho = 0; tamanho < 10; tamanho++){
-        soma += parseInt(cpf[tamanho]) * multiplicador;
-        multiplicador--;
-    }
-    soma = (soma * 10) % 11;
+for (let i = 0; i < 10; i++) {
+    soma += parseInt(cpf[i]) * multiplicador
+    multiplicador--
+}
+soma = (soma * 10) % 11;
 
-    if(soma == 10 || soma == 1){
-        soma = 0; 
-    }
-    return soma == parseInt(cpf[10]);
+if (soma == 10 || soma == 11) {
+    soma = 0;
+}
+return soma ==parseInt(cpf[10])
 }
